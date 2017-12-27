6eff4c7e82fbcb157bff241bf8a012b7c18db603
Hongde Liu <enginespot@gmail.com>
2017-12-27 15:10:17 +0800

    little drops make the mighty ocean

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp')
 var less = require('gulp-less')
 var path = require('path')
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
 
 gulp.task('less', function () {
+    var processors = [autoprefixer]
     gulp.src('./src/third-party/less/*.less')
         .pipe(less())
+        .pipe(postcss(processors))
         .pipe(gulp.dest('dist/styles'));
     gulp.src('./src/third-party/less/fonts/**/*')
         .pipe(gulp.dest('dist/fonts'));
